f9cf874e61d02a2ccbd579e61252683f68c11ce0
solbaby888 <lethuymy@live.com>
2016-04-10 11:50:53 -0400

    Added files via upload

---
+++ Final_Project.m
@@ -0,0 +1,18 @@
+% Final Project
+
+%% Import Data
+
+% Subject 1
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+
+% Subject 2
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+
+% Subject 3
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
