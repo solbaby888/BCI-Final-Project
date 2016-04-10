986501af4e04586a3f7156dba943998fe9ffa643
solbaby888 <lethuymy@live.com>
2016-04-10 17:40:32 -0400

    updated Final_Project Code

--- Final_Project.m
+++ Final_Project.m
@@ -3,16 +3,40 @@
 %% Import Data
 
 % Subject 1
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-
+sesh_sub1_1 = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub1_2 = IEEGSession('I521_A0012_D002', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub1_3 = IEEGSession('I521_A0012_D003', 'solbaby888', 'sol_ieeglogin.bin');
+nr_1        = ceil((sesh_sub1_1.data(1).rawChannels(11).get_tsdetails.getEndTime)/...
+                1e6*sesh_sub1_1.data(1).sampleRate);
 % Subject 2
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub2_1 = IEEGSession('I521_A0013_D001', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub2_2 = IEEGSession('I521_A0013_D002', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub2_3 = IEEGSession('I521_A0013_D003', 'solbaby888', 'sol_ieeglogin.bin');
+nr_2        = ceil((sesh_sub2_1.data(1).rawChannels(11).get_tsdetails.getEndTime)/...
+                1e6*sesh_sub2_1.data(1).sampleRate);
 
 % Subject 3
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub3_1 = IEEGSession('I521_A0014_D001', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub3_2 = IEEGSession('I521_A0014_D002', 'solbaby888', 'sol_ieeglogin.bin');
+sesh_sub3_3 = IEEGSession('I521_A0014_D003', 'solbaby888', 'sol_ieeglogin.bin');
+nr_3        = ceil((sesh_sub2_3data(1).rawChannels(11).get_tsdetails.getEndTime)/...
+                1e6*sesh_sub2_1.data(1).sampleRate);
+
+%{
+    TL_Comment: Loads pretty fast
+
+%}
+
+%% Check for 60 Hz noise
+
+Y = fft(Chan_11);
+L = length(Chan_11);
+f = fs*(0:(L/2))/L;
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+figure;
+plot(f,P1)
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
