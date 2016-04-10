91c0427200d8f21e93f89e2da41f5018becfe25f
solbaby888 <lethuymy@live.com>
2016-04-10 11:51:50 -0400

    Update Final_Project.m

--- Final_Project.m
+++ Final_Project.m
@@ -8,11 +8,11 @@ session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
 session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
 
 % Subject 2
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0013_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0013_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0013_D001', 'solbaby888', 'sol_ieeglogin.bin');
 
 % Subject 3
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
-session = IEEGSession('I521_A0012_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0014_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0014_D001', 'solbaby888', 'sol_ieeglogin.bin');
+session = IEEGSession('I521_A0014_D001', 'solbaby888', 'sol_ieeglogin.bin');
